d31cfc37f998dad2d652bd64edf94e2f8621ec3d
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2023-08-12 22:49:54 +0300

    Updater practice files

--- findMiddleIndexElement.js
+++ findMiddleIndexElement.js
@@ -1,4 +1,4 @@
-var gimme = function (inputArray) {
+var gimme = function(inputArray) {
 
   let sortedCopy = inputArray.slice().sort((a, b) => a - b); 
   return inputArray.indexOf(sortedCopy[1]); 
