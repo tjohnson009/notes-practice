4f18ec9f4573686394787f15b477dd231abd8c52
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-04-28 20:00:27 -0400

    Notes for output management

--- top-exercises/webpack-demo/src/index.js
+++ top-exercises/webpack-demo/src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash'; 
+import printMe from './print.js'; 
 
 
 function component() {
     const element = document.createElement('div');
+    const button = document.createElement('button'); 
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    button.innerHTML = 'Click me and check the console'
+    button.onclick = printMe; 
+
+    element.appendChild(button); 
     return element;
   }
   
--- top-exercises/webpack-demo/webpack.config.js
+++ top-exercises/webpack-demo/webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
   },
-  // mode: 'production'
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, 
+  },
+  mode: 'development', 
+
 };
